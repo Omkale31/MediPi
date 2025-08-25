074425d793cdddd773f98da5a32064a192b2f6f9
Omkale31 <130905559+Omkale31@users.noreply.github.com>
2025-08-25 22:14:48 +0530

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,16 @@
+from picamera2 import Picamera2
+import cv2
+import time
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration(main={"size": (640, 480)})
+picam2.configure(video_config)
+picam2.start()
+
+def generate_frames():
+    while True:
+        frame = picam2.capture_array()
+        _, buffer = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        time.sleep(0.1)
